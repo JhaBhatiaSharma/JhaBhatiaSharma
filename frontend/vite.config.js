953cdd5219db523104fc609f2b9254641765e731
shreeshjha <shreeshkumar2@gmail.com>
2025-01-31 15:40:27 +0100

    Fix base path for GitHub Pages deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,8 +2,13 @@ import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+// Check if running in GitHub Pages
+const isGitHubPages = process.env.GITHUB_PAGES === "true";
+
 export default defineConfig({
   plugins: [react()],
+  base: isGitHubPages ? "/JhaBhatiaSharma/" : "/",
+ // Set correct base path
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
