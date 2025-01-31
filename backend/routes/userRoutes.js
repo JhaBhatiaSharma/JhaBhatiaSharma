b976884682672a7a9bdf516d9cf43cf531468512
SamarthBhatia <samarthbhatia700@gmail.com>
2025-01-31 20:33:22 +0100

    fix: unused var fix in userRoutes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,6 +3,7 @@ const {
   resetPassword,
 } = require("../controllers/userController");
 const { getAllRecruiters } = require("../controllers/recruiterController");
+// eslint-disable-next-line no-unused-vars, unused-imports/no-unused-vars
 const { authMiddleware, roleMiddleware } = require("../middleware/authMiddleware");
 const router = express.Router();
 
